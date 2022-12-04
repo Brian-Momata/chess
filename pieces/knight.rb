87f47bc2da384bb9a9f29c1aca95a15c014b0ad1
Brian-Momata <brianmomata@gmail.com>
2022-12-04 08:31:58 +0300

    Add knight class

---
+++ pieces/knight.rb
@@ -0,0 +1,15 @@
+# class for a chess knight
+class knight
+  def directions
+    [
+      [1, 2],
+      [2, 1],
+      [-1, 2],
+      [-2, 1],
+      [1, -2],
+      [2, -1],
+      [-1, -2],
+      [-2, -1]
+    ]
+  end
+end
