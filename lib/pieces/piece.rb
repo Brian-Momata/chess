02f51f39cb7e424192a91b50859f752e15da99f8
Brian-Momata <brianmomata@gmail.com>
2022-12-12 08:49:36 +0300

    Add reference to the board

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -1,11 +1,18 @@
 # class for a chess piece
 class Piece
-  attr_reader :color
+  attr_reader :color, :board
   attr_accessor :location
 
-  def initialize(location, color)
+  def initialize(board, location, color)
     @location = location
     @color = color
+    @board = board
+  end
+
+  def enemy?(piece)
+    return false if piece.nil?
+
+    piece.color != color
   end
 
   def out_of_bounds?(position = location)
