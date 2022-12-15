59ee961ae5ace5cec319bbe6b62766f5c7b65e98
Brian-Momata <brianmomata@gmail.com>
2022-12-15 08:08:09 +0300

    Add unique #available_moves

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -3,7 +3,6 @@ require_relative 'stepable.rb'
 
 # class for pawn in chess
 class Pawn < Piece
-  include Stepable
 
   def directions
     if at_beginning? && color == :black
@@ -16,15 +15,44 @@ class Pawn < Piece
         [-1, 0],
         [-2, 0]
       ]
-    elsif color == :black
+    elsif color == :black && !at_beginning?
       [
         [1, 0]
       ]
-    elsif color == :white
+    elsif color == :white && !at_beginning?
       [
         [-1, 0]
       ]
     end
+  end
+
+  def available_moves
+    moves = []
+    row, col = location
+    directions.each do |(dr, dc)|
+      new_loc = [row + dr, col + dc]
+      break if out_of_bounds?(new_loc)
+
+      if board[new_loc].nil?
+        moves << new_loc unless !board[one_forward].nil?
+      end
+      if color == :black && enemy?(board[[row + 1, col - 1]])
+        moves << [row + 1, col - 1]
+      end
+
+      if color == :black && enemy?(board[[row + 1, col + 1]])
+        moves << [row + 1, col + 1]
+      end
+
+      if color == :white && enemy?(board[[row -1, col + 1]])
+        moves << [row -1, col + 1]
+      end
+
+      if color == :white && enemy?(board[[row -1, col - 1]])
+        moves << [row -1, col - 1]
+      end
+    end
+    moves
   end
 
   def to_s
@@ -36,5 +64,14 @@ class Pawn < Piece
   def at_beginning?
     row, col = location
     row == 1 || row == 6
+  end
+
+  def one_forward
+    r, c = location
+    if color == :black
+      [r + 1, c] 
+    else
+      [r - 1, c]
+    end
   end
 end
