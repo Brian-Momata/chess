2fbf133c8606e5fc0a5d3b66ace21bd4a9efcc1a
Brian-Momata <brianmomata@gmail.com>
2022-12-19 08:29:50 +0300

    Use next instead of break

--- lib/pieces/stepable.rb
+++ lib/pieces/stepable.rb
@@ -1,12 +1,12 @@
 # module for a chess piece that moves one square at a time
 module Stepable
+  
   def available_moves
-    # move forward if the space is empty/ has enemy piece
     moves = []
     row, col = location
     directions.each do |(dr, dc)|
       new_loc = [row + dr, col + dc]
-      break if out_of_bounds?(new_loc)
+      next if out_of_bounds?(new_loc)
 
       if board[new_loc].nil? || enemy?(board[new_loc])
         moves << new_loc
